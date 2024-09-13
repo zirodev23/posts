cfa997caaf2cfe45bb0919144ddb8bdc8ee48e74
unknown <your@email.example>
2024-09-13 13:10:22 +0300

    Posts are listed

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <h1>All posts</h1>
+    <ul>
+        @foreach($posts as $post)
+            <li>
+                <h2>Title: {{ $post->title }}</h2>
+                <p>Content: {{ $post->content }}</p>
+            </li>
+        @endforeach
+    </ul>
+</x-app-layout>
